6c5875a28308df6eea318180b6d722912f707b1f
Maurice Wallbott <maurice@wallbott.it>
2016-03-03 17:03:28 +0100

    createQuestion merge

--- arsnova.click/client/js/createQuestionView.js
+++ arsnova.click/client/js/createQuestionView.js
@@ -3,14 +3,14 @@
  */
 Template.createQuestionView.helpers({
     //Get question from Sessions-Collection if it already exists
-    question: function() {
+    question: function () {
         //To test
-        //Session.set("hashtag", "wpw");
-        //window.localStorage.setItem("privateKey", "thisismypriv");
+        Session.set("hashtag", "wpw");
+        window.localStorage.setItem("privateKey", "thisismypriv");
 
-        const currentSession = Sessions.findOne({hashtag:Session.get("hashtag")});
+        const currentSession = Sessions.findOne({hashtag: Session.get("hashtag")});
 
-        if(!currentSession || !currentSession.questionText) {
+        if (!currentSession || !currentSession.questionText) {
             return "";
         }
         return currentSession.questionText;
@@ -19,24 +19,22 @@ Template.createQuestionView.helpers({
 
 Template.createQuestionView.events({
     //Save question in Sessions-Collection when Button "Next" is clicked
-    "click #forwardButton":function(){
+    "click #forwardButton": function () {
         //Validate input?
-        const questionText = $('#questionText').val();
-
-        Meteor.call("setSessionQuestion", window.localStorage.getItem("privateKey"), Session.get("hashtag"), questionText);
+        var questionText = $('#questionText').val();
+        Meteor.call("setSessionQuestion", localStorage.getItem("privateKey"), Session.get("hashtag"), questionText);
     },
 
     //Save question in Sessions-Collection when Button "Back" is clicked
-    "click #backButton":function(){
+    "click #backButton": function () {
         //Validate input?
-        const questionText = $('#questionText').val();
-
-        Meteor.call("setSessionQuestion", window.localStorage.getItem("privateKey"), Session.get("hashtag"), questionText);
+        var questionText = $('#questionText').val();
+        Meteor.call("setSessionQuestion", localStorage.getItem("privateKey"), Session.get("hashtag"), questionText);
     },
-    "click #formatButton":function(){
+    "click #formatButton": function () {
         //Not implemented yet
     },
-    "click #previewButton":function(){
+    "click #previewButton": function () {
         //Not implemented yet
     }
 });
--- arsnova.click/shared/db.js
+++ arsnova.click/shared/db.js
@@ -1,12 +1,17 @@
 Meteor.methods({
-    setSessionQuestion:function(privateKey, hashtag, questionText){
-        const hashItem = Hashtags.findOne({hashtag:hashtag});
-        if(hashItem && privateKey === hashItem.privateKey) {
-            const session = Sessions.findOne({hashtag:hashtag});
-            if(!session) {
-                Sessions.insert({hashtag:hashtag, questionText:questionText, timer:20, isReadingConfirmationRequired:1});
+    setSessionQuestion: function (privateKey, hashtag, questionText) {
+        const hashItem = Hashtags.findOne({hashtag: hashtag});
+        if (hashItem && privateKey === hashItem.privateKey) {
+            const session = Sessions.findOne({hashtag: hashtag});
+            if (!session) {
+                Sessions.insert({
+                    hashtag: hashtag,
+                    questionText: questionText,
+                    timer: 20,
+                    isReadingConfirmationRequired: 1
+                });
             } else {
-                Sessions.update(session._id, {$set: {questionText: questionText}}, function(error) {
+                Sessions.update(session._id, {$set: {questionText: questionText}}, function (error) {
                     if (error) {
                         console.log(error);
                     }

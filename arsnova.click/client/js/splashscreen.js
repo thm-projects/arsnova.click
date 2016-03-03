185fcdd5070a4e26da3db8d1db9ecdc77f0dbf14
Michael <michael.sann92@gmail.com>
2016-03-03 23:35:45 +0100

    some refinements and splashscreen-scrollbar

--- arsnova.click/client/js/splashscreen.js
+++ arsnova.click/client/js/splashscreen.js
@@ -1,24 +1,23 @@
 /*kevin weigand, michael sann*/
 
 Template.splashscreen.rendered = function () {
-    $('.js-splashscreen').modal('show');
-    $('.js-splashscreen').on('click', function () {
+    $('.js-splashscreen').modal('show').on('click', function () {
         $('.js-splashscreen').modal('hide')
     });
 
     // test data in localStorage
     var qq = [
-        { content: "SWT 29.03" },
-        { content: "SWT 29.043" },
-        { content: "SWT 29.01233" },
-        { content: "SWT 29.12" }
+        {content: "SWT 29.03"},
+        {content: "SWT 29.043"},
+        {content: "SWT 29.01233"},
+        {content: "SWT 29.12"}
     ];
     localStorage.setItem("activeSessions", JSON.stringify(qq));
 
     var qq = [
-        { content: "Feuerball Junge" },
-        { content: "Stern" },
-        { content: "Framework" }
+        {content: "Feuerball Junge"},
+        {content: "Stern"},
+        {content: "Framework"}
     ];
     localStorage.setItem("answerExample", JSON.stringify(qq));
 
@@ -34,7 +33,5 @@ Template.sessionsT.helpers({
 
 Template.questionT.helpers({
     answ: JSON.parse(localStorage.getItem("answerExample")),
-    quest:localStorage.getItem("questionExample")
+    quest: localStorage.getItem("questionExample")
 });
-
-

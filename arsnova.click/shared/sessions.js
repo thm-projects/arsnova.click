1f46b37c3f91badd55a8e92f33ce4fad6606386e
Tjark Wilhelm Hoeck <Tjark Wilhelm Hoeck>
2016-03-04 21:47:13 +0100

    setReadingConfirmationRequired view and business logic for instructor and little bugfixes

--- arsnova.click/shared/sessions.js
+++ arsnova.click/shared/sessions.js
@@ -31,5 +31,31 @@ Meteor.methods({
                 });
             }
         }
+    },
+    "Sessions.updateIsReadConfirmationRequired": function ({privateKey, hashtag, isReadConfirmationRequired}) {
+        new SimpleSchema({
+            isReadConfirmationRequired: {
+                type: Number,
+                min: 0,
+                max: 1
+            }
+        }).validate({isReadConfirmationRequired: isReadConfirmationRequired});
+
+        var hashtagDoc = Hashtags.findOne({
+            hashtag: hashtag,
+            privateKey: privateKey
+        });
+        if (hashtagDoc) {
+            var session = Sessions.findOne({hashtag: hashtag});
+            if (!session) {
+                throw new Meteor.Error('Sessions.updateIsReadConfirmationRequired: no access to session');
+            } else {
+                Sessions.update(session._id, {$set: {isReadingConfirmationRequired: isReadConfirmationRequired}}, function (error) {
+                    if (error) {
+                        throw new Meteor.Error('Sessions.updateIsReadConfirmationRequired', error);
+                    }
+                });
+            }
+        }
     }
 });

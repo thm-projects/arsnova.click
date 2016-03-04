118adb4c65eb87213d22eb192d8e193501db4eb4
Tim Strietzel <email@tim-strietzel.de>
2016-03-04 11:56:27 +0100

    Initial Memberlist, w/o "show more" button / edit

--- arsnova.click/client/js/memberlist.js
+++ arsnova.click/client/js/memberlist.js
@@ -6,10 +6,34 @@ Template.memberlist.onCreated(function () {
 
 Template.memberlist.helpers({
     learners:function () {
-        var memberCount = MemberList.length;
-        return MemberList.find();
+        var returnMemberList = MemberList.find();
+        var memberCount = returnMemberList.length;
+
+        var viewPortHeigth = $(".contentPosition").height();
+        var viewPortWidth = $(".contentPosition").width();
+
+        // btnLearnerHeight muss hart hinterlegt werden / ggf anpassung an neue css klassen
+        var btnLearnerHeight = 54;
+
+        // breakpoint width: <= 945 --> one collumn
+        // breakpoint width: > 945 --> three collumns
+
+        var breakFactor = viewPortWidth < 945 ? 1 : 3;
+
+        console.log(" - - - - - - - - - - - - - - - - - ");
+        console.log("     viewPortHeigth : " + viewPortHeigth);
+        console.log("     viewPortWidth : " + viewPortWidth);
+        console.log("     btnLearnerHeight : " + btnLearnerHeight);
+        console.log("     memberCount : " + memberCount);
+        console.log("     breakFactor : " + breakFactor);
+        console.log(" - - - - - - - - - - - - - - - - - ");
+        console.log("");
+        console.log("");
+        console.log("");
+        return returnMemberList;
     },
-    isOwnerAndReadConfirmationNeeded:function () {
+
+    isOwnerAndReadConfirmationNeeded: function () {
         // TODO isReadConfirmation needed?
         return Session.get("isOwner");
     },
@@ -17,7 +41,8 @@ Template.memberlist.helpers({
         return Session.get("hashtag");
     },
     loops: function () {
-        // list for test purpose
+        // list for test purpose //
+        // TODO: Delete this list before push
         return [
             true,
             true,

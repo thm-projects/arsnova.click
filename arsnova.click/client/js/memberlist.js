cf8b2b83e654ee576fb77e599c19c456837014c6
Christopher Fullarton <trayhem@gmail.com>
2016-03-07 15:27:32 +0100

    Fix "show more" button list & rendering on load

--- arsnova.click/client/js/memberlist.js
+++ arsnova.click/client/js/memberlist.js
@@ -7,31 +7,21 @@ Template.memberlist.onCreated(function () {
         this.subscribe('Sessions.isReadingConfirmationRequired', Session.get("hashtag"));
     });
 
-});
-
-Template.memberlist.onCreated(function () {
     $(window).resize(function () {
-        var viewPortHeight = $(".contentPosition").height() - $('.learner-title').height() + 40;
-        var viewPortWidth = $(".contentPosition").width();
-
-        // btnLearnerHeight muss hart hinterlegt werden / ggf anpassung an neue css klassen
-        var btnLearnerHeight = 54;
-
-        var queryLimiter = Math.floor(viewPortHeight / btnLearnerHeight);
-        if (queryLimiter < 4) {
-            queryLimiter = 4;
-        }
-        if (viewPortWidth >= 768 && viewPortWidth < 1200) {
-            queryLimiter *= 3;
-        } else if (viewPortWidth >= 1200) {
-            queryLimiter *= 4;
-        }
-
-        Session.set("showMoreButton", queryLimiter);
-        console.log("Show Buttons: " + queryLimiter);
-    })
+        calculateButtonCount();
+    });
 });
 
+Template.memberlist.rendered = function () {
+    calculateButtonCount();
+}
+
+Template.memberlist.events({
+    "click .btn-more-learners": function (event) {
+        Session.set("LearnerCount", MemberList.find() + 1);
+        Session.set("LearnerCountOverride", true);
+    }
+});
 
 Template.memberlist.helpers({
     isOwner: function () {
@@ -49,12 +39,14 @@ Template.memberlist.helpers({
     },
 
     learners: function () {
-        var returnMemberList = MemberList.find({}, {limit: (Session.get("showMoreButton") - 1)});
-        return returnMemberList;
+        return MemberList.find({}, {
+            limit: (Session.get("LearnerCount") - 1),
+            sort: {nick: 1}
+        });
     },
 
     showMoreButton: function () {
-        return Session.get("showMoreButton") <= MemberList.find().count();
+        return Session.get("LearnerCount") <= MemberList.find().count();
     },
 
     isReadingConfirmationRequired: function () {
@@ -70,3 +62,28 @@ Template.memberlist.helpers({
         return Session.get("hashtag");
     }
 });
+
+function calculateButtonCount () {
+
+    if (Session.get("LearnerCountOverride")) {
+        return;
+    }
+
+    var viewPortHeight = $(".contentPosition").height() - $('.learner-title').height() + 40;
+    var viewPortWidth = $(".contentPosition").width();
+
+    // btnLearnerHeight muss hart hinterlegt werden / ggf anpassung an neue css klassen
+    var btnLearnerHeight = 54;
+
+    var queryLimiter = Math.floor(viewPortHeight / btnLearnerHeight);
+    if (queryLimiter < 4) {
+        queryLimiter = 4;
+    }
+    if (viewPortWidth >= 768 && viewPortWidth < 1200) {
+        queryLimiter *= 3;
+    } else if (viewPortWidth >= 1200) {
+        queryLimiter *= 4;
+    }
+
+    Session.set("LearnerCount", queryLimiter);
+}
--- arsnova.click/server/startup.js
+++ arsnova.click/server/startup.js
@@ -36,6 +36,66 @@ if(Meteor.isServer){
                 nick: "Deadpool",
                 readConfirmed: 0
             };
+            var memberListExample7 = {
+                hashtag: "wpw",
+                nick: "Nerd",
+                readConfirmed: 0
+            };
+            var memberListExample8 = {
+                hashtag: "wpw",
+                nick: "Iron Man",
+                readConfirmed: 0
+            };
+            var memberListExample9 = {
+                hashtag: "wpw",
+                nick: "Deadpool",
+                readConfirmed: 0
+            };
+            var memberListExample10 = {
+                hashtag: "wpw",
+                nick: "Testuser",
+                readConfirmed: 1
+            };
+            var memberListExample11 = {
+                hashtag: "wpw",
+                nick: "Metoo",
+                readConfirmed: 1
+            };
+            var memberListExample12 = {
+                hashtag: "wpw",
+                nick: "Nein",
+                readConfirmed: 0
+            };
+            var memberListExample13 = {
+                hashtag: "wpw",
+                nick: "Nerd",
+                readConfirmed: 0
+            };
+            var memberListExample14 = {
+                hashtag: "wpw",
+                nick: "Iron Man",
+                readConfirmed: 0
+            };
+            var memberListExample15 = {
+                hashtag: "wpw",
+                nick: "Deadpool",
+                readConfirmed: 0
+            };
+            var memberListExample16 = {
+                hashtag: "wpw",
+                nick: "Nerd",
+                readConfirmed: 0
+            };
+            var memberListExample17 = {
+                hashtag: "wpw",
+                nick: "Iron Man",
+                readConfirmed: 0
+            };
+            var memberListExample18 = {
+                hashtag: "wpw",
+                nick: "Deadpool",
+                readConfirmed: 0
+            };
             var sessionExample = {
                 hashtag: "wpw",
                 questionText: "I am a question text. This is for testing purpose. Do you understand?",
@@ -86,6 +146,18 @@ if(Meteor.isServer){
             MemberList.insert(memberListExample4);
             MemberList.insert(memberListExample5);
             MemberList.insert(memberListExample6);
+            MemberList.insert(memberListExample7);
+            MemberList.insert(memberListExample8);
+            MemberList.insert(memberListExample9);
+            MemberList.insert(memberListExample10);
+            MemberList.insert(memberListExample11);
+            MemberList.insert(memberListExample12);
+            MemberList.insert(memberListExample13);
+            MemberList.insert(memberListExample14);
+            MemberList.insert(memberListExample15);
+            MemberList.insert(memberListExample16);
+            MemberList.insert(memberListExample17);
+            MemberList.insert(memberListExample18);
         }
     });
 }

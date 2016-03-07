b9e6b9049ced016e3c13994e246e3a931c1fbcbb
Tjark Wilhelm Hoeck <tjark.wilhelm.hoeck@mni.thm.de>
2016-03-07 20:05:49 +0100

    sort answeroptions

--- arsnova.click/client/js/createansweroptions.js
+++ arsnova.click/client/js/createansweroptions.js
@@ -6,7 +6,7 @@ Template.createAnswerOptions.onCreated(function () {
 
 Template.createAnswerOptions.helpers({
    answerOptions: function () {
-      return AnswerOptions.find();
+      return AnswerOptions.find({}, {sort:{answerOptionNumber: 1}});
    },
    answerOptionLetter: function (Nr) {
       return String.fromCharCode(Nr + 65);

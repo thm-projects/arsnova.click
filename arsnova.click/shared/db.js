27120c08a3b081aa2a254b78418854169026738e
Kevin Linne <kevin.linne@mni.thm.de>
2016-03-08 11:55:50 +0100

    Meteor method and call for adding timer to DB

--- arsnova.click/shared/db.js
+++ arsnova.click/shared/db.js
@@ -1,18 +1,3 @@
 Meteor.methods({
-    setSessionTimer:function(privateKey, hashtag, timer){
-        const hashItem = Hashtags.findOne({hashtag:hashtag});
 
-        if(hashItem && privateKey === hashItem.privateKey) {
-            const session = Sessions.findOne({hashtag:hashtag});
-            if(!session) {
-                console.log("Error: No session found");
-            } else {
-                Sessions.update(session._id, {$set: {timer: timer}}, function(error) {
-                    if (error) {
-                        console.log(error);
-                    }
-                });
-            }
-        }
-    }
 });

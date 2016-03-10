cd627faac140fec663ec47a228215ec9920d8e0d
tekay <tom.kaesler@mni.thm.de>
2016-03-10 18:57:28 +0100

    add leaderboard with function to sort and fetch everything it needs.. \o/ add route

---
+++ arsnova.click/client/js/leaderboard.js
@@ -0,0 +1,52 @@
+Template.leaderBoard.onCreated(function () {
+    this.autorun(() => {
+        this.subscription = Meteor.subscribe('Responses.instructor', Session.get("hashtag"), function () {
+            Session.set("responsesLoaded", true);
+        });
+        this.subscription = Meteor.subscribe('AnswerOptions.options', Session.get("hashtag"), function () {
+            Session.set("answerOptionsLoaded", true);
+        });
+        this.subscription = Meteor.subscribe('MemberList.members', Session.get("hashtag"), function () {
+            Session.set("memberListLoaded", true);
+        });
+    });
+});
+
+Template.leaderBoard.helpers({
+    "leaderBoardItems": function () {
+        var leaderBoardItems = [];
+        var allGoodMembers = [];
+        var rightAnswerOptions = AnswerOptions.find({isCorrect: 1});
+        if (Session.get("responsesLoaded") && Session.get("memberListLoaded") && Session.get("answerOptionsLoaded")) {
+            var memberNicks = MemberList.find({}, {fields: {nick: 1}});
+            memberNicks.forEach(function (member) {
+                var entry = {
+                    nick: member.nick,
+                    responseTime: 0
+                };
+                var userResponses = Responses.find({userNick: member.nick});
+                var userHasRightAnswers = true;
+                // only put member in leaderboard when he clicked the right amount, then check whether he clicked all the right ones
+                var totalResponseTime = 0;
+                if ((userResponses.count() === rightAnswerOptions.count()) && (userResponses.count() > 0) && userHasRightAnswers ) {
+                    userResponses.forEach(function (userResponse) {
+                        var checkAnswerOptionDoc = AnswerOptions.findOne({isCorrect: 1, answerOptionNumber: userResponse.answerOptionNumber});
+                        if (!checkAnswerOptionDoc) {
+                            userHasRightAnswers = false;
+                        }
+                        else {
+                            totalResponseTime += userResponse.responseTime;
+                        }
+                    });
+                    if (userHasRightAnswers) {
+                        entry.responseTime = totalResponseTime / rightAnswerOptions.count();
+                        allGoodMembers.push(entry);
+                    }
+                }
+            });
+        }
+        var sortedArray = _.sortBy(allGoodMembers, 'responseTime');
+        console.log(sortedArray);
+        return sortedArray;
+    }
+});

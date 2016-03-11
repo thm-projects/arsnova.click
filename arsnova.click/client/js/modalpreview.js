379562397b4613468a2b05733c60f90e5d3d8cf6
Michael Sann <michael.sann92@gmail.com>
2016-03-11 14:07:54 +0100

    preview splash and refactoring

---
+++ arsnova.click/client/js/modalpreview.js
@@ -0,0 +1,8 @@
+Template.previewSplash.helpers({
+});
+
+Template.previewSplash.events({
+    "click #js-btn-hideAnswerModal": function () {
+        $('.js-splashscreen').modal("hide");
+    }
+});

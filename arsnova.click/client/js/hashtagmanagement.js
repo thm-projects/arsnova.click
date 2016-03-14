2dc81670e2af0ac0be818aa0d1f64a0f4e387cd3
tekay <tom.kaesler@mni.thm.de>
2016-03-14 20:22:13 +0100

    add some design get hashtag for events

--- arsnova.click/client/js/hashtagmanagement.js
+++ arsnova.click/client/js/hashtagmanagement.js
@@ -3,3 +3,21 @@ Template.hashtagManagement.onCreated(function () {
         this.subscribe('Hashtags.public');
     });
 });
+
+Template.hashtagManagement.helpers({
+    serverHashtags: function () {
+        return localData.getAllHashtags();
+    }
+});
+
+Template.hashtagManagement.events({
+    "click .js-reactivate-hashtag": function (event) {
+        var hashtag = $(event.currentTarget).parent().parent()[0].id;
+    },
+    "click .js-export": function (event) {
+        var hashtag = $(event.currentTarget).parent().parent()[0].id;
+    },
+    "click .js-delete": function (event) {
+        var hashtag = $(event.currentTarget).parent().parent()[0].id;
+    }
+});

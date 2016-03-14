481840bf318ea9d8fb7738e41e4dde617c290531
Tjark Wilhelm Hoeck <tjark.wilhelm.hoeck@mni.thm.de>
2016-03-14 11:01:50 +0100

    check if session is available for joining

--- arsnova.click/shared/memberlist.js
+++ arsnova.click/shared/memberlist.js
@@ -16,15 +16,19 @@ Meteor.methods({
             nick: nick
         });
         if (!member) {
-            MemberList.insert({
-                hashtag: hashtag,
-                nick: nick,
-                lowerCaseNick: nick.toLowerCase(),
-                backgroundColor: backgroundColor,
-                foregroundColor: foregroundColor,
-                readConfirmed: 0,
-                insertDate: new Date().getTime()
-            });
+            if (Hashtags.findOne({hashtag:hashtag}).sessionStatus == 2) {
+                MemberList.insert({
+                    hashtag: hashtag,
+                    nick: nick,
+                    lowerCaseNick: nick.toLowerCase(),
+                    backgroundColor: backgroundColor,
+                    foregroundColor: foregroundColor,
+                    readConfirmed: 0,
+                    insertDate: new Date().getTime()
+                });
+            } else {
+                throw new Meteor.error('MemberList.addLearner', 'Session is currently not available for joining');
+            }
         } else {
             throw new Meteor.Error('MemberList.addLearner', 'Nick already exists!');
             return;

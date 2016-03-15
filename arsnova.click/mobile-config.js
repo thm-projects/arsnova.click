2fd57716a503714ab036fdffb2914239ec3f8e2c
Daniel Henkel <daniel.henkel@match-maker.de>
2016-03-15 21:43:06 +0100

    started to add ios and android app

---
+++ arsnova.click/mobile-config.js
@@ -0,0 +1,19 @@
+App.info({
+    version: '1.0.0',
+    id: 'com.thm.arsnova.click',
+    name: 'ARSnova.click',
+    description: 'Quiz app fir alle(s) !',
+    email: 'info@arsnova.eu',
+    website: 'http://arsnova.click'
+});
+
+App.icons({
+    'iphone': 'public/images/icons/arsNovaClick-72.png',
+    'iphone_2x': 'public/images/icons/arsNovaClick-144.png'
+});
+
+/*App.launchScreens({
+    'iphone': 'resources/splash/Default~iphone.png',
+    'iphone_2x': 'resources/splash/Default@2x~iphone.png',
+    'iphone5': 'resources/splash/Default-568h@2x~iphone.png'
+});*/

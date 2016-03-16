852d14c12987246fd05fee7fc1d54c4561c4b0d5
Anton Schwarz <antonschwarz@wsp-design.de>
2016-03-16 14:11:03 +0100

    add icons for android devices and PlayStore icon

--- arsnova.click/mobile-config.js
+++ arsnova.click/mobile-config.js
@@ -2,14 +2,20 @@ App.info({
     version: '1.0.0',
     id: 'com.thm.arsnova.click',
     name: 'ARSnova.click',
-    description: 'Quiz app fir alle(s) !',
+    description: 'Quiz app für alle(s) !',
     email: 'info@arsnova.eu',
     website: 'http://arsnova.click'
 });
 
 App.icons({
     'iphone': 'public/images/icons/arsNovaClick-72.png',
-    'iphone_2x': 'public/images/icons/arsNovaClick-144.png'
+    'iphone_2x': 'public/images/icons/arsNovaClick-144.png',
+    'android_ldpi': 'public/images/icons/icon-ldpi.png',
+    'android_mdpi': 'public/images/icons/icon-mdpi.png',
+    'android_hdpi': 'public/images/icons/icon-hdpi.png',
+    'android_xhdpi': 'public/images/icons/icon-xhdpi.png',
+    'android_xxhdpi': 'public/images/icons/icon-xxhdpi.png',
+    'android_xxxhdpi': 'public/images/icons/icon-xxxhdpi.png'
 });
 
 /*App.launchScreens({

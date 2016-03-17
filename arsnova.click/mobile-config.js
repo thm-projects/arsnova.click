298c766c25773bacc1660acd7763376596d18ab4
Anton Schwarz <antonschwarz@wsp-design.de>
2016-03-17 19:42:28 +0100

    add icons and SplashScreens for android and ios devices

--- arsnova.click/mobile-config.js
+++ arsnova.click/mobile-config.js
@@ -8,16 +8,51 @@ App.info({
 });
 
 App.icons({
-    'iphone': 'public/images/icons/arsNovaClick-72.png',
-    'iphone_2x': 'public/images/icons/arsNovaClick-144.png',
-    'android_ldpi': 'public/images/icons/icon-ldpi.png',
-    'android_mdpi': 'public/images/icons/icon-mdpi.png',
-    'android_hdpi': 'public/images/icons/icon-hdpi.png',
-    'android_xhdpi': 'public/images/icons/icon-xhdpi.png'
+    'iphone': 'public/images/ios/icon_iphone_1x.png',
+    'iphone_2x': 'public/images/ios/icon_iphone_2x.png',
+    'iphone_3x': 'public/images/ios/icon_iphone_3x.png',
+
+    'ipad': 'public/images/ios/icon_ipad_1x.png',
+    'ipad_2': 'public/images/ios/icon_ipad_2x.png',
+
+    'android_ldpi': 'public/images/android/ldpi/icon-ldpi.png',
+    'android_mdpi': 'public/images/android/mdpi/icon-mdpi.png',
+
+    'android_hdpi': 'public/images/android/hdpi/icon-hdpi.png',
+    'android_xhdpi': 'public/images/android/xhdpi/icon-xhdpi.png',
+
+    'android_xxhdpi': 'public/images/android/xxhdpi/icon-xxhdpi.png',
+    'android_xxxhdpi': 'public/images/android/xxxhdpi/icon-xxxhdpi.png'
 });
 
-/*App.launchScreens({
-    'iphone': 'resources/splash/Default~iphone.png',
-    'iphone_2x': 'resources/splash/Default@2x~iphone.png',
-    'iphone5': 'resources/splash/Default-568h@2x~iphone.png'
-});*/
+App.launchScreens({
+    'iphone': 'public/images/ios/ls_iphone_portrait_1x.png',
+    'iphone_2x': 'public/images/ios/ls_iphone_portrait_2x.png',
+    'iphone5': 'public/images/ios/ls_iphone5_portrait_2x.png',
+    'iphone6': 'public/images/ios/ls_iphone6_portrait_2x.png',
+    'iphone6p_portrait': 'public/images/ios/ls_iphone6p_portrait_3x.png',
+    'iphone6p_landscape': 'public/images/ios/ls_iphone6p_landscape_3x.png',
+
+    'ipad_portrait': 'public/images/ios/ls_ipad_portrait_1x.png',
+    'ipad_portrait_2x': 'public/images/ios/ls_ipad_portrait_2x.png',
+    'ipad_landscape': 'public/images/ios/ls_ipad_landscape_1x.png',
+    'ipad_landscape_2x': 'public/images/ios/ls_ipad_landscape_2x.png',
+
+    'android_ldpi_portrait': 'public/images/android/ldpi/ls_ldpi_portrait.png',
+    'android_ldpi_landscape': 'public/images/android/ldpi/ls_ldpi_landscape.png',
+
+    'android_mdpi_portrait': 'public/images/android/mdpi/ls_mdpi_portrait.png',
+    'android_mdpi_landscape': 'public/images/android/mdpi/ls_mdpi_landscape.png',
+
+    'android_hdpi_portrait': 'public/images/android/hdpi/ls_hdpi_portrait.png',
+    'android_hdpi_landscape': 'public/images/android/hdpi/ls_hdpi_landscape.png',
+
+    'android_xhdpi_portrait': 'public/images/android/xhdpi/ls_xhdpi_portrait.png',
+    'android_xhdpi_landscape': 'public/images/android/xhdpi/ls_xhdpi_landscape.png',
+
+    'android_xxhdpi_portrait': 'public/images/android/xxhdpi/ls_xxhdpi_portrait.png',
+    'android_xxhdpi_landscape': 'public/images/android/xxhdpi/ls_xxhdpi_landscape.png',
+
+    'android_xxxhdpi_portrait': 'public/images/android/xxxhdpi/ls_xxxhdpi_portrait.png',
+    'android_xxxhdpi_landscape': 'public/images/android/xxxhdpi/ls_xxhdpi_landscape.png'
+});

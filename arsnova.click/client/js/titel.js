6581f7568e8cd8ce76a9a6d694beb877949161c4
Anton Schwarz <antonschwarz@wsp-design.de>
2016-03-23 11:59:13 +0100

    fixed the size of the hashtag in the header at teacher view by checking various size of the hashtag and set the margin of the hashtag dynamically, so that the hashtag ist in the middle of the logo.
     Also fixed the Kill-Switch on difference window width
    
     Please check before merge

--- arsnova.click/client/js/titel.js
+++ arsnova.click/client/js/titel.js
@@ -8,6 +8,8 @@ Template.titel.onRendered(function () {
         var final_height = $(window).height() - $(".navbar-fixed-top").outerHeight() - $(".navbar-fixed-bottom").outerHeight() - $(".fixed-bottom").outerHeight();
         $(".container").css("height", final_height);
         $(".container").css("margin-top", $(".navbar-fixed-top").outerHeight());
+
+        $(".kill-session-switch-wrapper").css("top", $(".arsnova-logo").height() * 0.4);
     });
 });
 
@@ -17,4 +19,6 @@ Template.titel.rendered = function () {
 
     $(".container").css("height", final_height);
     $(".container").css("margin-top", $(".navbar-fixed-top").outerHeight());
+
+    $(".kill-session-switch-wrapper").css("top", $(".arsnova-logo").height() * 0.4);
 };

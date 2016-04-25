12f3858070804358a37b3728e96524878e6f864f
Christopher Fullarton <christopher@fullarton.eu>
2016-04-25 14:36:41 +0200

    Fix jshint errors

--- arsnova.click/client/layout/view_leaderboard/scripts/lib.js
+++ arsnova.click/client/layout/view_leaderboard/scripts/lib.js
@@ -27,24 +27,48 @@ export function setMaxResponseButtons(value) {
     maxResponseButtons = value;
 }
 
-export function getLeaderBoardItems () {
-    if (typeof Session.get("showLeaderBoardId") !== "undefined") {
-        return [{value: getLeaderBoardItemsByIndex(Session.get("showLeaderBoardId"))}];
-    } else {
-        if (!EventManager.findOne()) {
-            return [];
-        }
+export function calculateButtonCount (allMembersCount) {
 
-        var result = [];
-        for (var i = 0; i <= EventManager.findOne().questionIndex; i++) {
-            result.push({
-                index: i,
-                value: getLeaderBoardItemsByIndex(i)
-            });
-        }
-
-        return result;
+    /*
+     This session variable determines if the user has clicked on the show-more-button. The button count must not
+     be calculated then. It is set in the event handler of the button and is reset if the user reenters the page
+     */
+    if (Session.get("responsesCountOverride")) {
+        setMaxResponseButtons(allMembersCount);
+        return;
     }
+
+    /*
+     To calculate the maximum output of attendee button rows we need to:
+     - get the mainContentContainer height (the content wrapper for all elements)
+     - subtract the appTitle height (the indicator for the question index)
+     */
+    var viewport = $('.container'),
+        appTitle = $('#appTitle');
+
+    var viewPortHeight = viewport.outerHeight() - appTitle.outerHeight();
+
+    /* The height of the learner button must be set manually if the html elements are not yet generated */
+    var btnLearnerHeight = $('.button-leader').first().parent().outerHeight(true) ? $('.button-leader').first().parent().outerHeight(true) : 70;
+
+    /* Calculate how much buttons we can place in the viewport until we need to scroll */
+    var queryLimiter = Math.floor(viewPortHeight / btnLearnerHeight);
+
+    /*
+     Multiply the displayed elements by 2 if on widescreen and reduce the max output of buttons by 1 row for the display
+     more button if necessary. Also make sure there is at least one row of buttons shown even if the user has to scroll
+     */
+    var limitModifier = viewport.outerWidth() >= 768 ? 2 : 1;
+
+    queryLimiter *= limitModifier;
+    if (queryLimiter <= 0) {
+        queryLimiter = limitModifier;
+    }
+
+    /*
+     This variable holds the amount of shown buttons and is used in the scripts functions
+     */
+    setMaxResponseButtons(queryLimiter);
 }
 
 function getLeaderBoardItemsByIndex(index) {
@@ -88,46 +112,22 @@ function getLeaderBoardItemsByIndex(index) {
     return _.sortBy(allGoodMembers, 'responseTime').slice(0, maxResponseButtons);
 }
 
-export function calculateButtonCount (allMembersCount) {
+export function getLeaderBoardItems () {
+    if (typeof Session.get("showLeaderBoardId") !== "undefined") {
+        return [{value: getLeaderBoardItemsByIndex(Session.get("showLeaderBoardId"))}];
+    } else {
+        if (!EventManager.findOne()) {
+            return [];
+        }
 
-    /*
-     This session variable determines if the user has clicked on the show-more-button. The button count must not
-     be calculated then. It is set in the event handler of the button and is reset if the user reenters the page
-     */
-    if (Session.get("responsesCountOverride")) {
-        setMaxResponseButtons(allMembersCount);
-        return;
+        var result = [];
+        for (var i = 0; i <= EventManager.findOne().questionIndex; i++) {
+            result.push({
+                index: i,
+                value: getLeaderBoardItemsByIndex(i)
+            });
+        }
+
+        return result;
     }
-
-    /*
-     To calculate the maximum output of attendee button rows we need to:
-     - get the mainContentContainer height (the content wrapper for all elements)
-     - subtract the appTitle height (the indicator for the question index)
-     */
-    var viewport = $('.container'),
-        appTitle = $('#appTitle');
-
-    var viewPortHeight = viewport.outerHeight() - appTitle.outerHeight();
-
-    /* The height of the learner button must be set manually if the html elements are not yet generated */
-    var btnLearnerHeight = $('.button-leader').first().parent().outerHeight(true) ? $('.button-leader').first().parent().outerHeight(true) : 70;
-
-    /* Calculate how much buttons we can place in the viewport until we need to scroll */
-    var queryLimiter = Math.floor(viewPortHeight / btnLearnerHeight);
-
-    /*
-     Multiply the displayed elements by 2 if on widescreen and reduce the max output of buttons by 1 row for the display
-     more button if necessary. Also make sure there is at least one row of buttons shown even if the user has to scroll
-     */
-    var limitModifier = viewport.outerWidth() >= 768 ? 2 : 1;
-
-    queryLimiter *= limitModifier;
-    if (queryLimiter <= 0) {
-        queryLimiter = limitModifier;
-    }
-
-    /*
-     This variable holds the amount of shown buttons and is used in the scripts functions
-     */
-    setMaxResponseButtons(queryLimiter);
 }

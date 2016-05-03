c65fbd0800cec7b77b684abad7e329bb5e77d34b
Christopher Fullarton <trayhem@gmail.com>
2016-05-03 16:16:54 +0200

    Debug abstract class variables

--- arsnova.click/lib/answeroptions/answeroption_abstract.js
+++ arsnova.click/lib/answeroptions/answeroption_abstract.js
@@ -1,9 +1,9 @@
 export class AbstractAnswerOption {
-    private var _hashtag = Symbol("_hashtag");
-    private var _questionIndex = Symbol("_questionIndex");
-    private var _answerText = Symbol("_answerText");
-    private var _answerOptionNumber = Symbol("_answerOptionNumber");
-    private var _isCorrect = Symbol("_isCorrect");
+    var hashtag = Symbol("hashtag");
+    var questionIndex = Symbol("questionIndex");
+    var answerText = Symbol("answerText");
+    var answerOptionNumber = Symbol("answerOptionNumber");
+    var isCorrect = Symbol("isCorrect");
 
     constructor () {
         if (new.target === AbstractAnswerOption) {
@@ -12,44 +12,44 @@ export class AbstractAnswerOption {
         if (!options.hashtag || !options.questionIndex || !options.answerText || !options.answerOptionNumber || !options.isCorrect) {
             throw new Error("Invalid argument list for AnswerOption instantiation");
         }
-        this._hashtag = options.hashtag;
-        this._questionIndex = options.questionIndex;
-        this._answerText = options.answerText;
-        this._answerOptionNumber = options.answerOptionNumber;
-        this._isCorrect = options.isCorrect;
+        this.hashtag = options.hashtag;
+        this.questionIndex = options.questionIndex;
+        this.answerText = options.answerText;
+        this.answerOptionNumber = options.answerOptionNumber;
+        this.isCorrect = options.isCorrect;
     }
 
     public getHashtag () {
-        return this._hashtag;
+        return this.hashtag;
     }
 
     public getQuestionIndex () {
-        return this._questionIndex;
+        return this.questionIndex;
     }
 
     public getAnswerText () {
-        return this._answerText;
+        return this.answerText;
     }
 
     public setAnswerText (text) {
         if (!text || !text instanceof String) {
             throw new Error("Invalid argument for AnswerOption.setAnswerText");
         }
-        this._answerText = text;
+        this.answerText = text;
     }
 
     public getAnswerOptionNumber () {
-        return this._answerOptionNumber;
+        return this.answerOptionNumber;
     }
 
     public getIsCorrect () {
-        return this._isCorrect;
+        return this.isCorrect;
     }
 
     public setIsCorrect (isCorrect) {
         if (!isCorrect || !isCorrect instanceof Boolean) {
             throw new Error("Invalid argument for AnswerOption.setIsCorrect");
         }
-        this._isCorrect = isCorrect;
+        this.isCorrect = isCorrect;
     }
 }

c49ded16c8db46780277b40c4f7786604edc38d3
Christopher Fullarton <christopher@fullarton.eu>
2016-05-06 12:15:00 +0200

    Fix gulp errors

--- arsnova.click/client/layout/view_hashtag_management/scripts/events.js
+++ arsnova.click/client/layout/view_hashtag_management/scripts/events.js
@@ -14,7 +14,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with ARSnova Click.  If not, see <http://www.gnu.org/licenses/>.*/
+
 import {Meteor} from 'meteor/meteor';
+import {Session} from 'meteor/session';
 import {Template} from 'meteor/templating';
 import {TAPi18n} from 'meteor/tap:i18n';
 import {EventManagerCollection} from '/lib/eventmanager/collection.js';
--- arsnova.click/client/layout/view_timer/scripts/onRendered.js
+++ arsnova.click/client/layout/view_timer/scripts/onRendered.js
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with ARSnova Click.  If not, see <http://www.gnu.org/licenses/>.*/
 
+import {Session} from 'meteor/session';
 import {Template} from 'meteor/templating';
 import {Tracker} from 'meteor/tracker';
 import {EventManagerCollection} from '/lib/eventmanager/collection.js';

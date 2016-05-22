8595376b4bdbd3b49987876b88df67f689b03c37
Christopher Fullarton <christopher@fullarton.eu>
2016-05-22 09:16:53 +0200

    Add doc for answerOptions

--- arsnova.click/lib/answeroptions/answeroption_abstract.js
+++ arsnova.click/lib/answeroptions/answeroption_abstract.js
@@ -6,6 +6,13 @@ const isCorrect = Symbol("isCorrect");
 
 export class AbstractAnswerOption {
 
+	/**
+	 * Constructor super method for creating a AnswerOption instance
+	 * This method cannot be invoked directly.
+	 * @param {{hashtag:String,questionIndex:Number,answerText:String,answerOptionNumber:Number,isCorrect:Boolean,type:Number}} options An object containing the parameters for creating an AnswerOption instance. The type attribute is optional.
+	 * @throws {TypeError} If this method is invoked directly, the options Object is undefined or the optional type attribute is not matching the constructor name
+	 * @throws {Error} If the hashtag, the questionIndex, the answerText, the answerOptionNumber or the isCorrect attributes of the options Object are missing
+	 */
 	constructor (options) {
 		if (this.constructor === AbstractAnswerOption) {
 			throw new TypeError("Cannot construct Abstract instances directly");
@@ -23,18 +30,35 @@ export class AbstractAnswerOption {
 		this[isCorrect] = options.isCorrect;
 	}
 
+	/**
+	 * Returns the hashtag identifying the corresponding session
+	 * @returns {String} The hashtag of the session this AnswerOption instance belongs to
+	 */
 	getHashtag () {
 		return this[hashtag];
 	}
 
+	/**
+	 * Returns the questionIndex this AnswerOption belongs to
+	 * @returns {Number} The question index
+	 */
 	getQuestionIndex () {
 		return this[questionIndex];
 	}
 
+	/**
+	 * Returns the currently set answer text
+	 * @returns {String} The answer text which will be displayed during a quiz
+	 */
 	getAnswerText () {
 		return this[answerText];
 	}
 
+	/**
+	 * Sets the answer text for this AnswerOption instance
+	 * @param {String} text The text which shall be displayed during a quiz
+	 * @throws {Error} If the text is not of type String
+	 */
 	setAnswerText (text) {
 		if (typeof text !== "string") {
 			throw new Error("Invalid argument for AnswerOption.setAnswerText");
@@ -42,14 +66,27 @@ export class AbstractAnswerOption {
 		this[answerText] = text;
 	}
 
+	/**
+	 * Returns the answerOptionNumber identifying this AnswerOption instance
+	 * @returns {Number} The answerOptionNumber of this AnswerOption instance
+	 */
 	getAnswerOptionNumber () {
 		return this[answerOptionNumber];
 	}
 
+	/**
+	 * Returns if this AnswerOption instance is currently marked as correct
+	 * @returns {Boolean} True if this AnswerOption instance is marked as correct, False otherwise
+	 */
 	getIsCorrect () {
 		return this[isCorrect];
 	}
 
+	/**
+	 * Set the correct-mark for this AnswerOption instance
+	 * @param {Boolean} value True, if this AnswerOption shall be marked as correct, False otherwise
+	 * @throws {Error} If the value is not of type Boolean
+	 */
 	setIsCorrect (value) {
 		if (typeof value !== "boolean") {
 			throw new Error("Invalid argument for AnswerOption.setIsCorrect");
@@ -57,6 +94,10 @@ export class AbstractAnswerOption {
 		this[isCorrect] = value;
 	}
 
+	/**
+	 * Serialized the instance object to a JSON compatible object
+	 * @returns {{hashtag: String, type: String, questionIndex: Number, answerText: String, answerOptionNumber: Number, isCorrect: Boolean}}
+	 */
 	serialize () {
 		return {
 			hashtag: this.getHashtag(),
@@ -68,10 +109,20 @@ export class AbstractAnswerOption {
 		};
 	}
 
+	/**
+	 * Checks if the properties of this instance are valid.
+	 * @returns {boolean} True, if the complete Question instance is valid, False otherwise
+	 */
 	isValid () {
 		return this.getAnswerText().length > 0;
 	}
 
+	/**
+	 * Checks for equivalence relations to another AnswerOption instance. Also part of the EJSON interface
+	 * @see http://docs.meteor.com/api/ejson.html#EJSON-CustomType-equals
+	 * @param {AbstractAnswerOption} answerOption The AnswerOption instance which should be checked
+	 * @returns {boolean} True if both instances are completely equal, False otherwise
+	 */
 	equals (answerOption) {
 		return answerOption instanceof AbstractAnswerOption &&
 			answerOption.getQuestionIndex() === this.getQuestionIndex() &&
@@ -80,10 +131,21 @@ export class AbstractAnswerOption {
 			answerOption.getIsCorrect() === this.getIsCorrect();
 	}
 
+	/**
+	 * Part of EJSON interface.
+	 * @see http://docs.meteor.com/api/ejson.html#EJSON-CustomType-typeName
+	 * @returns {String} The name of the instantiated subclass
+	 */
 	typeName () {
 		return this.constructor.name;
 	}
 
+	/**
+	 * Part of EJSON interface
+	 * @see AbstractAnswerOption.serialize()
+	 * @see http://docs.meteor.com/api/ejson.html#EJSON-CustomType-toJSONValue
+	 * @returns {{hashtag: String, questionIndex: Number, answerText: String, answerOptionNumber: Number, isCorrect: Boolean}}
+	 */
 	toJSONValue () {
 		return {
 			hashtag: this.getHashtag(),
--- arsnova.click/lib/answeroptions/answeroption_default.js
+++ arsnova.click/lib/answeroptions/answeroption_default.js
@@ -3,15 +3,29 @@ import {AbstractAnswerOption} from './answeroption_abstract.js';
 
 export class DefaultAnswerOption extends AbstractAnswerOption {
 
+	/**
+	 * Constructs a DefaultAnswerOption instance
+	 * @see AbstractAnswerOption.constructor()
+	 * @param options
+	 */
 	constructor (options) {
 		super(options);
 	}
 
+	/**
+	 * Part of EJSON interface
+	 * @see http://docs.meteor.com/api/ejson.html#EJSON-clone
+	 * @returns {DefaultAnswerOption} An independent deep copy of the current instance
+	 */
 	clone () {
 		return new DefaultAnswerOption(this.serialize());
 	}
 }
 
+/**
+ * Adds a custom type to Meteor's EJSON
+ * @see http://docs.meteor.com/api/ejson.html#EJSON-addType
+ */
 EJSON.addType("DefaultAnswerOption", function (value) {
 	return new DefaultAnswerOption(value);
 });

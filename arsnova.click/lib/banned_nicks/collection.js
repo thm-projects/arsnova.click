b94910b09778df50b00cf865fe107a7be7d58f7d
Christopher Fullarton <christopher@fullarton.eu>
2016-05-23 13:19:11 +0200

    Add collection for banned nicknames

---
+++ arsnova.click/lib/banned_nicks/collection.js
@@ -0,0 +1,54 @@
+/*
+ * This file is part of ARSnova Click.
+ * Copyright (C) 2016 The ARSnova Team
+ *
+ * ARSnova Click is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ARSnova Click is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARSnova Click.  If not, see <http://www.gnu.org/licenses/>.*/
+
+import {Mongo} from 'meteor/mongo';
+import {SimpleSchema} from 'meteor/aldeed:simple-schema';
+import {userNickSchema} from '../member_list/collection.js';
+import * as localData from '/lib/local_storage.js';
+
+export const BannedNicks = new Mongo.Collection("bannedNicks");
+export const bannedNicksCollectionSchema = new SimpleSchema({
+	userNick: {
+		type: userNickSchema
+	}
+});
+
+BannedNicks.attachSchema(bannedNicksCollectionSchema);
+
+BannedNicks.deny({
+	insert: function () {
+		return true;
+	},
+	update: function () {
+		return true;
+	},
+	remove: function () {
+		return true;
+	}
+});
+
+BannedNicks.allow({
+	insert: function (userId, doc) {
+		return localData.containsHashtag(doc.hashtag);
+	},
+	update: function (userId, doc) {
+		return localData.containsHashtag(doc.hashtag);
+	},
+	remove: function (userId, doc) {
+		return localData.containsHashtag(doc.hashtag);
+	}
+});

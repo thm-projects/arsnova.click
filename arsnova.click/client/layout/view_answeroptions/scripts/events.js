f1da31d68bae75bea2a00028f85f03c0dd4b4b15
Christopher Fullarton <christopher@fullarton.eu>
2016-05-25 11:31:24 +0200

    Fix issue when tabbing new answeroptions

--- arsnova.click/client/layout/view_answeroptions/scripts/events.js
+++ arsnova.click/client/layout/view_answeroptions/scripts/events.js
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with ARSnova Click.  If not, see <http://www.gnu.org/licenses/>.*/
 
+import {Meteor} from 'meteor/meteor';
 import {Session} from 'meteor/session';
 import {Template} from 'meteor/templating';
 import {EventManagerCollection} from '/lib/eventmanager/collection.js';
@@ -92,8 +93,15 @@ Template.createAnswerOptions.events({
 			if (nextElement.length <= 0) {
 				event.preventDefault();
 				$("#addAnswerOption").click();
-				//sets focus to the new input field
-				$(event.currentTarget).closest(".form-group").next().find(".input-field").focus();
+				//sets focus to the new input field - The field is not added instantly because of the sync to the localStorage so we need a small timeout here
+				const renderTimeoutFunction = function () {
+					if ($(event.currentTarget).closest(".form-group").next().find(".input-field").length > 0) {
+						$(event.currentTarget).closest(".form-group").next().find(".input-field").focus();
+					} else {
+						Meteor.setTimeout(renderTimeoutFunction, 20);
+					}
+				};
+				Meteor.setTimeout(renderTimeoutFunction, 20);
 			}
 		}
 	},

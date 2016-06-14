55d95d4866fd27327db7ad3e7ee7fb81de62d48a
Tjark Wilhelm Hoeck <tjark.wilhelm.hoeck@mni.thm.de>
2016-06-14 22:11:15 +0200

    theme sugar

--- arsnova.click/client/layout/view_theme_switcher/scripts/lib.js
+++ arsnova.click/client/layout/view_theme_switcher/scripts/lib.js
@@ -120,5 +120,10 @@ export const themes = [
 		name: "view.theme_switcher.themes.colored.name",
 		description: "view.theme_switcher.themes.colored.description",
 		id: "theme-colored"
+	},
+	{
+		name: "view.theme_switcher.themes.sugar.name",
+		description: "view.theme_switcher.themes.sugar.description",
+		id: "theme-sugar"
 	}
 ];

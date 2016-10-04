2406e547bc1fe707d842bdc8976bdccf6ec25630
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2016-10-04 15:03:16 +0200

    Fix jscs error

--- arsnova.click/client/layout/region_header/lib.js
+++ arsnova.click/client/layout/region_header/lib.js
@@ -46,7 +46,7 @@ export function calculateTitelHeight() {
 	var fixedTop = $(".navbar-fixed-top");
 	var container = $(".container");
 	var footerHeight = $(".fixed-bottom").outerHeight(true) + $(".footer-info-bar").outerHeight();
-	var navbarFooterHeight = $('.navbar-fixed-bottom').is(":visible") ? $(".navbar-fixed-bottom").outerHeight() : 0;
+	//var navbarFooterHeight = $('.navbar-fixed-bottom').is(":visible") ? $(".navbar-fixed-bottom").outerHeight() : 0;
 	var finalHeight = $(window).height() - fixedTop.outerHeight() - footerHeight;
 
 	container.css("height", finalHeight);

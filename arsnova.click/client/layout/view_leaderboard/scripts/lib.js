cd2b0a4dd80107b0a366680b3f990e037bb5cb2e
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2016-10-19 11:45:09 +0200

    Fix jscs errors

--- arsnova.click/client/layout/view_leaderboard/scripts/lib.js
+++ arsnova.click/client/layout/view_leaderboard/scripts/lib.js
@@ -160,14 +160,14 @@ function getAllNonPollingLeaderBoardItems() {
  */
 function dynamicSort(property) {
 	var sortOrder = 1;
-	if(property[0] === "-") {
+	if (property[0] === "-") {
 		sortOrder = -1;
 		property = property.substr(1);
 	}
 	return function (a,b) {
 		var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
 		return result * sortOrder;
-	}
+	};
 }
 
 export function getAllNicksWhichAreAlwaysRight() {

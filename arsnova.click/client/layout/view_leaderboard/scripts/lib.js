8534982a88bf1bfa505841abc8f6b1ee543ee05c
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2016-10-19 11:41:04 +0200

    Add sort function to the global leaderboard

--- arsnova.click/client/layout/view_leaderboard/scripts/lib.js
+++ arsnova.click/client/layout/view_leaderboard/scripts/lib.js
@@ -152,6 +152,24 @@ function getAllNonPollingLeaderBoardItems() {
 	return result;
 }
 
+/**
+ * Sorts an object by its property value
+ * @see http://stackoverflow.com/a/4760279
+ * @param property
+ * @returns {Function}
+ */
+function dynamicSort(property) {
+	var sortOrder = 1;
+	if(property[0] === "-") {
+		sortOrder = -1;
+		property = property.substr(1);
+	}
+	return function (a,b) {
+		var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+		return result * sortOrder;
+	}
+}
+
 export function getAllNicksWhichAreAlwaysRight() {
 	var leaderBoardItems = getAllNonPollingLeaderBoardItems();
 	var amountQuestions = EventManagerCollection.findOne().questionIndex + 1;
@@ -193,5 +211,5 @@ export function getAllNicksWhichAreAlwaysRight() {
 			}
 		}
 	});
-	return allTimeWinners;
+	return allTimeWinners.sort(dynamicSort("sumResponseTime"));
 }

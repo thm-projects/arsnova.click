5dd3f422a25df8c3e181138971ec631a2a6c21a7
cmfl37 <christopher.fullarton@mni.thm.de>
2016-10-29 13:21:05 +0200

    Add a confirmation splashscreen when editing the quiz

--- arsnova.click/client/layout/region_footer/scripts/events.js
+++ arsnova.click/client/layout/region_footer/scripts/events.js
@@ -333,9 +333,18 @@ const clickEvents = {
 		Router.go("/" + Router.current().params.quizName + "/nicknameCategories");
 	},
 	"click #edit-quiz": function () {
-		Meteor.call("MemberListCollection.removeFromSession", Router.current().params.quizName);
-		Meteor.call('EventManagerCollection.setSessionStatus', Router.current().params.quizName, 1);
-		Router.go("/" + Router.current().params.quizName + "/question");
+		new Splashscreen({
+			autostart: true,
+			templateName: "editSessionSplashscreen",
+			closeOnButton: '#closeDialogButton, #editSessionButton',
+			onRendered: function (instance) {
+				instance.templateSelector.find('#editSessionButton').on('click', function () {
+					Meteor.call("MemberListCollection.removeFromSession", Router.current().params.quizName);
+					Meteor.call('EventManagerCollection.setSessionStatus', Router.current().params.quizName, 1);
+					Router.go("/" + Router.current().params.quizName + "/question");
+				});
+			}
+		});
 	},
 	"click #reading-confirmation": function () {
 		const elem = $('#reading-confirmation').find(".footerElemIcon").find(".glyphicon");

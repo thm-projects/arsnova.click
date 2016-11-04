9e0f56f74bec7ec63e0e070088e7eb7503cd8db5
cmfl37 <christopher.fullarton@mni.thm.de>
2016-11-04 01:54:40 +0100

    Create package with recent version of jquery's qr code plugin

---
+++ arsnova.click/packages/jquery-qrcode/jquery-qrcode.js
@@ -0,0 +1,8 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See nouislider-tests.js for an example of importing.
+
+import * as jqueryQRCodeLib from './v0.14.0.min.js';
+
+export var qrCode = jqueryQRCodeLib;

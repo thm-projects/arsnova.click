ac23dce89fd976f35a0b598c6eeee0f87f54917c
cmfl37 <christopher.fullarton@mni.thm.de>
2016-11-07 10:47:41 +0100

    Fix jscs errors

--- arsnova.click/client/plugins/splashscreen/scripts/events.js
+++ arsnova.click/client/plugins/splashscreen/scripts/events.js
@@ -16,7 +16,7 @@
  * along with ARSnova Click.  If not, see <http://www.gnu.org/licenses/>.*/
 
 import {Template} from 'meteor/templating';
-import {showFullscreenPicture, closeSplashscreen} from './lib.js';
+import {showFullscreenPicture} from './lib.js';
 
 Template.questionAndAnswerSplashscreen.events({
 	"click .resizeableImage": function (event) {

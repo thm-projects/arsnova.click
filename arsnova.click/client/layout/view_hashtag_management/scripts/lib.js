22be56659afbaab9876b2761fb4e91173ac84b51
cmfl37 <christopher.fullarton@mni.thm.de>
2016-11-12 21:19:42 +0100

    Add sorting for demo quizzes

--- arsnova.click/client/layout/view_hashtag_management/scripts/lib.js
+++ arsnova.click/client/layout/view_hashtag_management/scripts/lib.js
@@ -60,7 +60,7 @@ export function findOriginalHashtag(inputHashtag) {
 }
 
 export function getNewDemoQuizName() {
-	const hashtags = HashtagsCollection.find({hashtag: {$regex: "demo quiz *", $options: 'i'}}).fetch();
+	const hashtags = HashtagsCollection.find({hashtag: {$regex: "demo quiz *", $options: 'i'}}, {$sort: {hashtag: 1}}).fetch();
 	const newIndex = hashtags.length === 0 ? 1 : parseInt(hashtags[hashtags.length - 1].hashtag.split(" ")[2]) + 1;
 	console.log(newIndex, hashtags);
 	return "Demo Quiz " + newIndex;

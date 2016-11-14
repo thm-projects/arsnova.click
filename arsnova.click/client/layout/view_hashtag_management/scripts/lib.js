8cd05a50a70836731dd2a40d6ba299ca02b64110
cmfl37 <christopher.fullarton@mni.thm.de>
2016-11-14 09:21:47 +0100

    Debug demo quizzes on the master server

--- arsnova.click/client/layout/view_hashtag_management/scripts/lib.js
+++ arsnova.click/client/layout/view_hashtag_management/scripts/lib.js
@@ -61,6 +61,7 @@ export function findOriginalHashtag(inputHashtag) {
 
 export function getNewDemoQuizName() {
 	const hashtags = HashtagsCollection.find({hashtag: {$regex: "demo quiz *", $options: 'i'}}, {sort: {hashtag: 1}}).fetch();
+	console.log(hashtags);
 	const newIndex = hashtags.length === 0 ? 1 : parseInt(hashtags[hashtags.length - 1].hashtag.split(" ")[2]) + 1;
 	return "Demo Quiz " + newIndex;
 }

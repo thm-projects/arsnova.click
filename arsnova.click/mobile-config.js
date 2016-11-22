17e23c14399bc9d1be966bb9662e0b978f4ce26e
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2016-11-22 15:52:49 +0100

    Bump mobile version

--- arsnova.click/mobile-config.js
+++ arsnova.click/mobile-config.js
@@ -16,7 +16,7 @@
  * along with ARSnova Click.  If not, see <http://www.gnu.org/licenses/>.*/
 
 App.info({
-	version: '1.1.3',
+	version: '1.1.4',
 	id: 'click.arsnova',
 	name: 'ARSnova.click',
 	description: 'Quiz-App für alle(s) !',

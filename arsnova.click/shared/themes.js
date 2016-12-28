59e5de3d23a065f0f6c93919050ac14ecbb9e4e1
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel@mni.thm.de>
2016-12-28 18:59:50 +0100

    Edit theme Materia Design

--- arsnova.click/shared/themes.js
+++ arsnova.click/shared/themes.js
@@ -5,6 +5,11 @@ export const themes = [
 		description: "view.theme_switcher.themes.theme-arsnova-dot-click-contrast.description",
 		id: "theme-arsnova-dot-click-contrast"
 	},
+  {
+    name: "view.theme_switcher.themes.material.name",
+    description: "view.theme_switcher.themes.material.description",
+    id: "theme-Material"
+  },
 	{
 		name: "view.theme_switcher.themes.black_beauty.name",
 		description: "view.theme_switcher.themes.black_beauty.description",
@@ -44,10 +49,5 @@ export const themes = [
 		name: "view.theme_switcher.themes.living_room.name",
 		description: "view.theme_switcher.themes.living_room.description",
 		id: "theme-hell"
-	},
-	{
-		name: "view.theme_switcher.themes.material.name",
-		description: "view.theme_switcher.themes.material.description",
-		id: "theme-Material"
 	}
 ];

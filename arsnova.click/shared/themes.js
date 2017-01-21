7eae4ff6990764109f7aeaf3a0fdc796906bcd50
Joshua Sattler <joshua.sattler@mni.thm.de>
2017-01-21 20:48:21 +0100

    repository up to date

--- arsnova.click/shared/themes.js
+++ arsnova.click/shared/themes.js
@@ -24,6 +24,11 @@ export const themes = [
 		name: "view.theme_switcher.themes.material_hope.name",
 		description: "view.theme_switcher.themes.material_hope.description",
 		id: "theme-Material-hope"
+	},
+    {
+		name: "view.theme_switcher.themes.decent_blue.name",
+		description: "view.theme_switcher.themes.decent_blue.description",
+		id: "theme-decent-blue"
 	}
 ];
 

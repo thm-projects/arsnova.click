0275a56f2a2c9d468479c8a80de894975fa07fd5
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel@mni.thm.de>
2017-01-22 16:33:29 +0100

    Adapt colors to theme Material

--- arsnova.click/packages/introJs/package.js
+++ arsnova.click/packages/introJs/package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.add_files('./lib/intro.js/intro.js', 'client');
   api.add_files('./lib/intro.js/introjs.css', 'client');
-  api.add_files('./lib/intro.js/themes/introjs-nazanin.css', 'client');
+  api.add_files('./lib/intro.js/themes/introjs-nassim.css', 'client');
   api.add_files('./lib/main.js', 'client');
   api.export('introJs', 'client');
 });

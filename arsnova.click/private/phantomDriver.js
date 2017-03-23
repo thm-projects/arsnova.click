b33482822952bf0d9a5f2d7556905f644eb5ed60
Christopher Fullarton <trayhem@gmail.com>
2017-03-23 08:36:39 +0100

    Start debugging of phantomjs screenshot generation

--- arsnova.click/private/phantomDriver.js
+++ arsnova.click/private/phantomDriver.js
@@ -1,6 +1,6 @@
 const system = require('system'),
-	  args = system.args,
-	  page = require("webpage").create();
+	args = system.args,
+	page = require("webpage").create();
 
 page.viewportSize = {
 	width: 1280,
@@ -9,14 +9,14 @@ page.viewportSize = {
 page.onConsoleMessage = function (message) {
 	console.log('Received message from phantomjs process:', message);
 };
-function handle_page(url){
+function handlePage(url) {
 	console.log("Requesting page load:", url);
-	page.open(url, function(status) {
+	page.open(url, function (status) {
 		const urlSeparated = url.split("/");
-		page.onCallback = function() {
+		page.onCallback = function () {
 			console.log('Page', url, 'is loaded and ready with status:', status);
 			page.render('arsnova_click_preview_' + urlSeparated[urlSeparated.length - 2] + '_' + urlSeparated[urlSeparated.length - 1] + '.png');
-			setTimeout(next_page, 100);
+			setTimeout(nextPage, 100);
 		};
 		setTimeout(function () {
 			page.evaluate(function () {
@@ -25,22 +25,22 @@ function handle_page(url){
 		}, 800);
 	});
 }
-function next_page(){
-	const url=args.shift();
-	if (!url){
+function nextPage() {
+	const url = args.shift();
+	if (!url) {
 		console.log("All files have been handled, exiting process");
 		phantom.exit(0);
 	}
 	const host = /localhost/.test(url) ? "localhost" : /staging.arsnova.click/.test(url) ? "staging.arsnova.click" : /arsnova.click/.test(url) ? "arsnova.click" : "";
 	phantom.addCookie({
-		'name'     : 'cookieconsent_dismissed',
-		'value'    : 'yes',
-		'domain'   : host,
-		'path'     : '/',
-		'expires'  : (new Date()).getTime() + (1000 * 60 * 60)   /* <-- expires in 1 hour */
+		'name': 'cookieconsent_dismissed',
+		'value': 'yes',
+		'domain': host,
+		'path': '/',
+		'expires': (new Date()).getTime() + (1000 * 60 * 60)   /* <-- expires in 1 hour */
 	});
 	console.log("Handling url", url, "from args", args);
-	handle_page(url);
+	handlePage(url);
 }
 args.shift();
-next_page();
+nextPage();
